7815789836be48d8ca7102634f547a85cf65b682
Mumarkzx <mumarques03@gmail.com>
2026-04-08 12:12:17 -0300

    adicionando header

--- sprint-3/src/components/Header.tsx
+++ sprint-3/src/components/Header.tsx
@@ -7,24 +7,12 @@ export default function Header() {
 
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sobre">Sobre</Link>
-          </li>
-          <li>
-            <Link to="/contato">Contato</Link>
-          </li>
-          <li>
-            <Link to="/faq">FAQ</Link>
-          </li>
-          <li>
-            <Link to="/integrantes">Integrantes</Link>
-          </li>
-          <li>
-            <Link to="/solucao">Solução</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+          <li><Link to="/contato">Contato</Link></li>
+          <li><Link to="/faq">FAQ</Link></li>
+          <li><Link to="/integrantes">Integrantes</Link></li>
+          <li><Link to="/solucao">Solução</Link></li>
         </ul>
       </nav>
     </header>
