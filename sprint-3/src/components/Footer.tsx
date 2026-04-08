a732fa9a2b799066aebc60bdccb92678545ddb01
luizkichimoto <luizkichimoto@gmail.com>
2026-04-08 12:09:38 -0300

    adicionando o footer

---
+++ sprint-3/src/components/Footer.tsx
@@ -0,0 +1,39 @@
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="https://www.facebook.com/turmadobem/?locale=pt_BR"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/ongturmadobem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/c/F%C3%A1bioBibancos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouTube
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <p>&copy; 2025. Turma do Bem - Home</p>
+    </footer>
+  );
+}
